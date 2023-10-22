99d01d6ee70c7fb29948171aee9b83f922e13ebb
James-Randall-14 <randallj24@student.jhs.net>
2023-10-22 00:09:10 -0700

    midnight coding at kins house

--- pilot_gui/find_cameras.py
+++ pilot_gui/find_cameras.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
+import flask
+from core.srv import AddCamera
 
 class Camera_Finder(Node):
 
@@ -10,6 +11,38 @@ class Camera_Finder(Node):
         self.log = self.get_logger() # Quick reference for logging
 
         self.known_ips = [] # Index of ips that are already recognized and added
+
+        # Set up the add camera service
+        self.client = self.create_client(AddCamera, 'AddCamera')
+        self.request = AddCamera.Request() 
+    
+    def add_cams(self, new_ip):
+        self.request.ip = str(new_ip)
+        self.future = self.client.call_async(self.req)
+
+    def find_cameras(self):
+
+        # Create an application with flask
+        app = flask.Flask(__name__)
+
+        # Write the Flask Callback
+        # Yes, I know it's gross to define a function within a function.
+        # There are cleaner ways to do this, but this is intelligible and works well.
+
+        @app.route('/', methods=["POST", "GET"])
+        def page():
+            incoming_ip = flask.request.remote_addr
+            incoming_form = flask.request.form
+
+            # If the IP is new, add it to known_ips and send it out to the camera viewer.
+            if incoming_ip not in self.known_ips and len(incoming_form) > 0:
+                self.log.info("Detected new camera. IP: {}".format(incoming_ip))
+                self.known_ips.append(incoming_ip)
+                self.add_cams(incoming_ip)
+
+            return ""
+        
+        app.run(host='0.0.0.0', port=12345)
 
 
 def main(args=None):
--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'find_cameras = pilot_gui.find_cameras:main',
         ],
     },
 )
