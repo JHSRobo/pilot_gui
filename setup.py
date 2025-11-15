32de37c3a0c305150992fd24c54ba514d909df9e
jack-frings <jackdfrings@protonmail.com>
2025-11-14 19:20:26 -0800

    changing maintainer information

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='jhsrobo',
-    maintainer_email='fringsj26@student.jhs.net',
+    maintainer_email='jackdfrings@protonmail.com',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
